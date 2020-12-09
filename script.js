f0e3d93ee5b403220d3f66a1138b67bf1f4918cb
joelsprangers <68640121+joelsprangers@users.noreply.github.com>
2020-12-09 16:23:26 +0100

    create rest and spread functions

--- script.js
+++ script.js
@@ -1,65 +1,16 @@
 
-const testNum = num => {
-    return new Promise((resolve, reject) => {
-        if (num > 10) {
-            resolve(num + ' is greater than 10');
-        } else {
-            reject(num + ' is less than 10');
-        }
-    });
-};
+const sumReduce = (...numbers) => numbers.reduce((acc, currentValue) => acc + currentValue)
 
-testNum(9)
-    .then(result => console.log(result))
-    .catch(error => console.log(error));
+let sum = sumReduce(2, 3, 4, 5, 6);
 
-testNum(11)
-    .then(result => console.log(result))
-    .catch(error => console.log(error));
+console.log(sum);
 
 
-//exc 2
 
-const makeAllCaps = (words) => {
-    return new Promise((resolve, reject) => {
-      if (
-        words.every(word => {
-          return typeof word === 'string';
-        })
-      ) {
-        resolve(
-          sortWords(
-            words.map(word => {
-              return word.toUpperCase();
-            })
-          )
-        );
-      } else {
-        reject('Not a string!');
-      }
-    });
-  };
-  
-  const sortWords = (words) => {
-    return new Promise((resolve, reject) => {
-      if (words) {
-        resolve(words.sort());
-      } else {
-        reject('strings only!');
-      }
-    });
-  };
-  
-  const theseAreWords = ['promise', 'practice', 'break'];
-  
-  makeAllCaps(theseAreWords)
-    .then(sortWords(theseAreWords))
-    .then(result => console.log(result))
-    .catch(error => console.log(error));
-  
-  const theseAreNotWords = [1, 'hello', 9];
-  
-  makeAllCaps(theseAreNotWords)
-    .then(sortWords(theseAreNotWords))
-    .then(result => console.log(result))
-    .catch(error => console.log(error));
+const cijfers = [1, 2, 4, 5, 6, 7]
+
+const sumSpread = (q, w, e, r, t, y) => q + w + e + r + t + y;
+
+const sumArray = sumSpread(...cijfers);
+
+console.log(sumArray);
